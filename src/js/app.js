ec89496e2b3b240569e7ecc2df8e6fcff7dd83d3
Enokkio <Enokfu@gmail.com>
2022-10-03 14:51:55 +0200

    functioning  ish?

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,35 @@
 
 import {BITMAP} from "./BITMAP.js"
 import {Plane} from "./PlaneClass.js"
+let b= 0;
+let firstLayerPlane = new Plane();
+var canvas = document.getElementById("canvas")
+var ctx = canvas.getContext("2d")
+function display(plane){
 
-let bitmap = new BITMAP();
-let plane = new Plane();
+    for (let i = 0; i < plane.plane.length; i++) {
+        console.log(plane.plane[i])
+        if (plane.plane[i] != undefined) {
+            ctx.fillRect(((i%10)*51),(Math.floor((i/10))*51),50,50)
 
-plane.putPixel(2,2, 255)
-console.table(bitmap.plane)
+            
+        }
+    }
 
+
+
+
+}
+
+function addpixel(){
+    firstLayerPlane.putPixel(b,b,255);
+    b++;
+
+}
+setInterval(() => {
+    addpixel();
+}, 10);
+
+setInterval(() => {
+    display(firstLayerPlane)
+}, 10);
