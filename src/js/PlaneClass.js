5f4959b19c734a854608f80e918bc9acc1093c63
Linus Andersson <linus.andersson17@learnet.se>
2022-10-05 12:50:10 +0200

    line complete

--- src/js/PlaneClass.js
+++ src/js/PlaneClass.js
@@ -1,42 +1,54 @@
 import { BITMAP } from "./BITMAP.js";
 export class Plane {
     constructor() {
-        this.width = 100;
-        this.height = 100;
+        this.width = 1000;
+        this.height = 1000;
         this.plane = new Array(this.height * this.width);
         this.lock = false;
         this.zoom = 1;
     }
     putPixel(x, y, color ) {
         this.plane[x + y*this.width] = color;
-        console.log(this.plane)
     }
     line(x1, y1, x2, y2, color) {
-
-        let distance
-
-        if(x2-x1 > y2-y1){
-            distance = x2
-        }else{
-            distance = y2
-        }
-
-        let k = ((y2-y1)/(x2-x1))
-        let step = 0
-        let count = 0
-        this.plane[x1+y1*this.width] = color
-        this.plane[x2+y2*this.width] = color
-
-        console.log(k);
-        for (let i = x1; i < distance; i++) {
-            
-            this.plane[x1+i*this.width+step] = color
-            count++
-            if(count >= k){
-                step++
-                count = 0
+            var dx = Math.abs(x2 - x1);
+            var dy = Math.abs(y2 - y1);
+            var sx = (x1 < x2) ? 1 : -1;
+            var sy = (y1 < y2) ? 1 : -1;
+            var err = dx - dy;
+         
+            while(true) {
+               this.putPixel(x1, y1,254); // Do what you need to for this
+         
+               if ((x1 === x2) && (y1 === y2)) break;
+               var e2 = 2*err;
+               if (e2 > -dy) { err -= dy; x1  += sx; }
+               if (e2 < dx) { err += dx; y1  += sy; }
             }
-        }
+        // let distance
+
+        // if(x2-x1 > y2-y1){
+        //     distance = x2
+        // }else{
+        //     distance = y2
+        // }
+
+        // // let k = ((y2-y1)/(x2-x1))
+        // let k = 1.25
+        // let step = 0
+        // let count = 0
+        // this.plane[x1+y1*this.width] = color
+        // this.plane[x2+y2*this.width] = color    
+        // console.log(k);
+        // for (let i = x1; i < distance; i++) {
+            
+        //     this.plane[x1+i*this.width+step] = color
+        //     count ++
+        //     if(count >= k){
+        //         step++
+        //         count = 0
+        //     }
+        // }
     }
     circle(x, y, r, color) {
     }
