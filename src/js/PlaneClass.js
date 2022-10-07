a82bd374b7b7eba456141ba40d30a78b581ab635
Linus Andersson <sunil.andersson@gmail.com>
2022-10-07 21:07:35 +0200

    scroll inte perserve

--- src/js/PlaneClass.js
+++ src/js/PlaneClass.js
@@ -1,98 +1,119 @@
 import { BITMAP } from "./BITMAP.js";
 export class Plane {
-    constructor() {
-        this.width = 40;
-        this.height = 40;
-        this.plane = new Array(this.height * this.width);
-        this.lock = false;
-    }
-    putPixel(x, y, color ) {
-        this.plane[x + y*this.width] = color;   
-    }
-    line(x1, y1, x2, y2, color) {
-            var dx = Math.abs(x2 - x1);
-            var dy = Math.abs(y2 - y1);
-            var sx = (x1 < x2) ? 1 : -1;
-            var sy = (y1 < y2) ? 1 : -1;
-            var err = dx - dy;
-         
-            while(true) {
-               this.putPixel(x1, y1,254); // Do what you need to for this
-         
-               if ((x1 === x2) && (y1 === y2)) break;
-               var e2 = 2*err;
-               if (e2 > -dy) { err -= dy; x1  += sx; }
-               if (e2 < dx) { err += dx; y1  += sy; }
-            }
-    }
-    circle(x, y, r, color, accuracy) {
-        
-        //center of circle (x,y)
-        //range of circle is (r)
-        //accurancy = sin(angle)*r, and cos(angle)*r
-        //point of change is 2pi/accuracy
-        let pointofchange=((2*3.141592)/(accuracy))
+  constructor() {
+    this.width = 40;
+    this.height = this.width;
+    this.plane = new Array(this.height * this.width);
+    this.lock = false;
+  }
+  putPixel(x, y, color) {
+    this.plane[x + y * this.width] = color;
+  }
+  line(x1, y1, x2, y2, color) {
+    var dx = Math.abs(x2 - x1);
+    var dy = Math.abs(y2 - y1);
+    var sx = x1 < x2 ? 1 : -1;
+    var sy = y1 < y2 ? 1 : -1;
+    var err = dx - dy;
 
-        for (let i = 0; i < accuracy; i++) {
-            let x1 =Math.ceil(x+r*Math.sin(pointofchange*i))
-             let y1 =  Math.ceil(y+r*Math.cos(pointofchange*i))
-             let x2 = Math.ceil(x+r*Math.sin(pointofchange*(i+1)))
-             let y2 = Math.ceil(y+r*Math.cos(pointofchange*(i+1)))
-            // this.putPixel(x1,y1,color)
-            // console.log("x coord: " + x1);
-            // console.log("y coord: " + y);
-            this.line(x1,y1,x2,y2,color)
+    while (true) {
+      this.putPixel(x1, y1, 254); // Do what you need to for this
 
-        }
+      if (x1 === x2 && y1 === y2) break;
+      var e2 = 2 * err;
+      if (e2 > -dy) {
+        err -= dy;
+        x1 += sx;
+      }
+      if (e2 < dx) {
+        err += dx;
+        y1 += sy;
+      }
+    }
+  }
+  circle(x, y, r, color, accuracy) {
+    //center of circle (x,y)
+    //range of circle is (r)
+    //accurancy = sin(angle)*r, and cos(angle)*r
+    //point of change is 2pi/accuracy
+    let pointofchange = (2 * 3.141592) / accuracy;
 
+    for (let i = 0; i < accuracy; i++) {
+      let x1 = Math.ceil(x + r * Math.sin(pointofchange * i));
+      let y1 = Math.ceil(y + r * Math.cos(pointofchange * i));
+      let x2 = Math.ceil(x + r * Math.sin(pointofchange * (i + 1)));
+      let y2 = Math.ceil(y + r * Math.cos(pointofchange * (i + 1)));
+      // this.putPixel(x1,y1,color)
+      this.line(x1, y1, x2, y2, color);
     }
-    rectangle(x1, y1, x2, y2, color) {
-        //four points to generate
-        //first point (x1,y1)
-        //second point (x2, y1)
-        //third point (x2,y2)
-        //fourth point (x1,y2)
-        this.line(x1,y1,x2,y1, color)
-        this.line(x2,y1,x2,y2,color)
-        this.line(x2,y2,x1,y2, color)
-        this.line(x1,y2,x1,y1, color)
+  }
+  rectangle(x1, y1, x2, y2, color) {
+    //four points to generate
+    //first point (x1,y1)
+    //second point (x2, y1)
+    //third point (x2,y2)
+    //fourth point (x1,y2)
+    this.line(x1, y1, x2, y1, color);
+    this.line(x2, y1, x2, y2, color);
+    this.line(x2, y2, x1, y2, color);
+    this.line(x1, y2, x1, y1, color);
+  }
+  clear(color) {
+    this.plane.forEach((element) => {
+      element = color;
+    });
+  }
+  resize(x, y) {
+    this.width = x;
+    this.height = y;
+    this.plane = [this.width * this.height];
+  }
 
+  textOut(x, y, color, txt) {}
 
+  scrollLeft() {
+    for (let y = 0; y < this.height; y++) {
+      for (let i = 0; i < this.plane.length / this.width; i++) {
+        if (i < this.width - 1)
+          this.plane[i + y * this.height] = this.plane[i + y * this.height + 1];
+        else this.plane[i + y * this.height] = undefined;
+      }
+    }
+    console.table(this.plane);
+  }
+  scrollRight() {
+    for (let y = 0; y < this.height; y++) {
+      for (let i = this.plane.length / this.width - 1; i >= 0; i--) {
+        if (i != 0)
+          this.plane[i + y * this.height] = this.plane[i + y * this.height - 1];
+        else this.plane[i + y * this.height] = undefined;
+      }
+    }
+  }
+  scrollUp() {
+    for (let y = 0; y < this.height; y++) {
+      for (let i = 0; i < this.width; i++)
+        this.plane[i + y * this.width] =
+          this.plane[i + y * this.width + this.height];
+    }
+  }
+  scrollDown() {
+    for (let y = this.height - 1; y >= 0; y--) {
+      for (let i = 0; i < this.width; i++) {
+        if (y == 39) this.plane[i + y * this.width] = undefined;
+        else
+          this.plane[i + y * this.width] = this.plane[i + y * this.width - 40];
+      }
+    }
+  }
+  pScrollLeft() {}
+  pScrollRight() {}
+  pScrollUp() {}
+  pScrollDown() {}
 
-    }
-    clear(color) {
-        this.plane.forEach(element => {
-            element = color
-        });
-    }
-    resize(x, y) {
-
-    }
-    textOut(x, y, color, txt) {
-    }
-    scrollLeft() {
-    }
-    scrollRight() {
-    }
-    scrollUp() {
-    }
-    scrollDown() {
-    }
-    pScrollLeft() {
-    }
-    pScrollRight() {
-    }
-    pScrollUp() {
-    }
-    pScrollDown() {
-    }
-    triangle(x1, y1, x2, y2, x3, y3, color) {
-        this.line(x1,y1,x2,y2, color)
-        this.line(x2,y2,x3,y3, color)
-        this.line(x3,y3,x1,y1, color)
-
-
-
-
-    }
+  triangle(x1, y1, x2, y2, x3, y3, color) {
+    this.line(x1, y1, x2, y2, color);
+    this.line(x2, y2, x3, y3, color);
+    this.line(x3, y3, x1, y1, color);
+  }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,38 +1,62 @@
-
-import {BITMAP} from "./BITMAP.js"
-import {Plane} from "./PlaneClass.js"
-let pixelWidth
+import { BITMAP } from "./BITMAP.js";
+import { Plane } from "./PlaneClass.js";
+let pixelWidth;
 let firstLayerPlane = new Plane();
-const canvas = document.getElementById("canvas")
-const ctx = canvas.getContext("2d")
-function display(plane){
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-    for (let i = 0; i < plane.plane.length; i++) {
-        
-        if (plane.plane[i] != undefined) {
-            ctx.fillRect(((i%plane.width)*(pixelWidth)),(Math.floor((i/plane.width))*(pixelWidth)),pixelWidth,pixelWidth )
-
-            
-        }
+const btnUp = document.getElementById("up")
+const btnDown = document.getElementById("down")
+const btnLeft = document.getElementById("left")
+const btnRight = document.getElementById("right")
+const btnPUp = document.getElementById("Pup")
+const btnPDown = document.getElementById("Pdown")
+const btnPLeft = document.getElementById("pPleft")
+const btnPRight = document.getElementById("Pright")
+function display(plane) {
+  for (let i = 0; i < plane.plane.length; i++) {
+    if (plane.plane[i] != undefined) {
+      ctx.fillRect(
+        (i % plane.width) * pixelWidth,
+        Math.floor(i / plane.width) * pixelWidth,
+        pixelWidth,
+        pixelWidth
+      );
     }
-
-
-
-
+  }
 }
 
-function reScale(){ 
-        pixelWidth = canvas.offsetHeight / firstLayerPlane.width;
-        canvas.width = canvas.offsetHeight
-        canvas.height = canvas.offsetHeight 
+function reScale() {
+  pixelWidth = canvas.offsetHeight / firstLayerPlane.width;
+  canvas.width = canvas.offsetHeight;
+  canvas.height = canvas.offsetHeight;
 }
 
-firstLayerPlane.circle(19,19,10,255,16)
-firstLayerPlane.line(0,0,39,39)
-firstLayerPlane.triangle(20,0,39,39,0,39)
-
-
+firstLayerPlane.line(0, 0, 39, 0);
+firstLayerPlane.line(0, 1, 39, 1);
+/*firstLayerPlane.line(0, 2, 39, 2);
+firstLayerPlane.line(0, 3, 39, 3);*/
+firstLayerPlane.line(0, 4, 39, 4);
+firstLayerPlane.line(0, 5, 39, 5);
+//firstLayerPlane.line(0, 6, 39, 6);
+firstLayerPlane.line(0, 39, 39, 39); 
 setInterval(() => {
-    reScale()
-    display(firstLayerPlane)
+  reScale();
+  display(firstLayerPlane);
 }, 50);
+
+
+btnLeft.addEventListener("click", () =>{
+    firstLayerPlane.scrollLeft()
+} )
+
+btnRight.addEventListener("click", () => {
+    firstLayerPlane.scrollRight()
+})
+
+btnUp.addEventListener("click", () => {
+    firstLayerPlane.scrollUp()
+})
+btnDown.addEventListener("click", () => {
+    firstLayerPlane.scrollDown()
+})
